9596bd7c833995d24b1119d78dc04c286772bc5b
Javier Sebastian <u20211e429@upc.edu.pe>
2023-09-26 19:01:41 -0500

    Add: register-form

--- XComponent/src/router/index.js
+++ XComponent/src/router/index.js
@@ -5,7 +5,7 @@ import ProductoCard from '../components/producto-card.component.vue'
 import HomeView from '../views/home-view.vue'
 import car from '../components/car.component.vue'
 import plan from '../components/plan.vue' 
-
+import register from '../components/register-form.component.vue'
 
 
 const routes = [
@@ -35,8 +35,12 @@ const routes = [
       path: '/plan',
       name: 'plan',
       component: plan,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: register,
     }
-  
  
 ]
   
